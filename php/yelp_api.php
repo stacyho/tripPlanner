951c92e39cf62281096ca4cda3b579d9b35421d3
Stacy Ho <stacyho@mit.edu>
2016-06-18 13:21:12 -0500

    Yelp API

---
+++ php/yelp_api.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Yelp API v2.0 code sample.
+ *
+ * This program demonstrates the capability of the Yelp API version 2.0
+ * by using the Search API to query for businesses by a search term and location,
+ * and the Business API to query information about a specific business.
+ * 
+ * Please refer to http://www.yelp.com/developers/documentation for the API documentation.
+ * 
+ * This program requires a PHP OAuth2 library.
+ * 
+ */
+
+
+// Enter the path that the oauth library is in relation to the php file
+require_once('OAuth.php');
+// Set your OAuth credentials here  
+// These credentials can be obtained from the 'Manage API Access' page in the
+// developers documentation (http://www.yelp.com/developers)
+$CONSUMER_KEY = 'jaWpeN5_ut3jT956QZYoaA';
+$CONSUMER_SECRET = 'FeYKa3ARBpi2jLjQRpQJQ5UK9Q8';
+$TOKEN = 'QrJTyNo9s3IqN8RdbsqPxmYVqcBv5n7h';
+$TOKEN_SECRET = 'D-h33fklBMx5Ik8iEk1F9ol7_sg';
+$API_HOST = 'api.yelp.com';
+$DEFAULT_TERM = 'restaurants';
+$DEFAULT_LOCATION = 'San Francisco, CA';
+$SEARCH_LIMIT = 5;
+$SEARCH_PATH = '/v2/search/';
+$BUSINESS_PATH = '/v2/business/';
+/** 
+ * Makes a request to the Yelp API and returns the response
+ * 
+ * @param    $host    The domain host of the API 
+ * @param    $path    The path of the APi after the domain
+ * @return   The JSON response from the request      
+ */
+function request($host, $path) {
+
+    $unsigned_url = "https://" . $host . $path;
+    // Token object built using the OAuth library
+    $token = new OAuthToken($GLOBALS['TOKEN'], $GLOBALS['TOKEN_SECRET']);
+    // Consumer object built using the OAuth library
+    $consumer = new OAuthConsumer($GLOBALS['CONSUMER_KEY'], $GLOBALS['CONSUMER_SECRET']);
+    // Yelp uses HMAC SHA1 encoding
+    $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
+    $oauthrequest = OAuthRequest::from_consumer_and_token(
+        $consumer, 
+        $token, 
+        'GET', 
+        $unsigned_url
+    );
+    
+    // Sign the request
+    $oauthrequest->sign_request($signature_method, $consumer, $token);
+    
+    // Get the signed URL
+    $signed_url = $oauthrequest->to_url();
+    
+    // Send Yelp API Call
+    try {
+        $ch = curl_init($signed_url);
+        if (FALSE === $ch)
+            throw new Exception('Failed to initialize');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt ($ch, CURLOPT_CAINFO, 'C:\Users\Stacy\Documents\Project\tripPlanner\php\cacert.pem');
+        $data = curl_exec($ch);
+        if (FALSE === $data)
+            throw new Exception(curl_error($ch), curl_errno($ch));
+        $http_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if (200 != $http_status)
+            throw new Exception($data, $http_status);
+        curl_close($ch);
+    } catch(Exception $e) {
+        echo sprintf('Curl failed with error #%d: %s',$e->getCode(), $e->getMessage());
+    }
+    
+    return $data;
+}
+/**
+ * Query the Search API by a search term and location 
+ * 
+ * @param    $term        The search term passed to the API 
+ * @param    $location    The search location passed to the API 
+ * @return   The JSON response from the request 
+ */
+function search($term, $location) {
+    $url_params = array();
+    
+    $url_params['term'] = $term ?: $GLOBALS['DEFAULT_TERM'];
+    $url_params['location'] = $location?: $GLOBALS['DEFAULT_LOCATION'];
+    $url_params['limit'] = $GLOBALS['SEARCH_LIMIT'];
+    $search_path = $GLOBALS['SEARCH_PATH'] . "?" . http_build_query($url_params);
+    
+    return request($GLOBALS['API_HOST'], $search_path);
+}
+/**
+ * Query the Business API by business_id
+ * 
+ * @param    $business_id    The ID of the business to query
+ * @return   The JSON response from the request 
+ */
+function get_business($business_id) {
+    $business_path = $GLOBALS['BUSINESS_PATH'] . urlencode($business_id);
+    
+    return request($GLOBALS['API_HOST'], $business_path);
+}
+
+
+?>
